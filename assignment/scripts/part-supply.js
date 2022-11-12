fe97cf2ebd701320f430537033de77b0f328739c
Farah Mire <farah_mire@yahoo.com>
2022-11-11 22:43:58 -0500

    first changes

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -15,18 +15,28 @@ console.log(partsNeeded);
 //    the following numbers: 3, 5, -6, 0, 7, 11
 console.log('2. Array of supplyChanges:');
 
+let supplyChanges  = [ 3, 5, -6, 0, 7, 11 ];
+console.log('Array of supplyChanges:', supplyChanges);
+
 // 3. Console log the value of the second item in the 'supplyChanges' array
 console.log('3. Second supplyChange is:');
+
+console.log('Second supplyChange is:', supplyChanges[1]);
+
 
 // 4. The last item was added by mistake. Remove it from the 'supplyChanges' 
 //    array & console.log the value removed.
 console.log('4. Removed item:');
 
-
+supplyChanges.splice(1,1);
+console.log(supplyChanges);
 //use .splice or .pop
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Adding 25 to supplyChanges.');
+
+supplyChanges.push(25);
+console.log(supplyChanges);
 
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
@@ -34,6 +44,19 @@ console.log('5. Adding 25 to supplyChanges.');
 //    - if the value is 0, log 'No Change.'
 //    - if the value is negative, format the log as 'Removed x parts.' 
 console.log('6. Showing supplyChanges...');
+
+x = [ 3, 5, -6, 0, 7, 11 ];
+
+for (let x=0; x < supplyChanges.length; x++) { 
+    console.log(supplyChanges[x]); 
+
+    if (x >= 1) { 
+    console.log('Added x parts:'); 
+}   else if ( x === 0) {   
+    console.log( 'No Change'); 
+}   else if ( x < 0 ) { 
+    console.log( 'Removed x parts:'); 
+}
 
 
 
